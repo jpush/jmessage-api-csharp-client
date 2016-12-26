0ee7ac755c36cc786b6ffb39ffb21253d752308e
fendouai <fendouai@gmail.com>
2016-12-26 17:48:46 +0800

    add more field of user

--- jmessage/user/UserPayload.cs
+++ jmessage/user/UserPayload.cs
@@ -12,14 +12,46 @@ namespace jmessage.user
     {
         public string username;
         public string password;
+        public string appkey;
+        public string nickname;
+        public string birthday;
+        public int? gender;
+        public string signature;
+        public string region;
+        public string address;
+        public string ctime;
+        public string mtime;
+
+
         public UserPayload(string username, string password)
         {
             this.username = username;
             this.password = password;
         }
+        public UserPayload()
+        {
+            this.username = null;
+            this.password = null;
+            this.appkey = null;
+            this.nickname = null;
+            this.birthday = null;
+            this.gender = null;
+            this.signature = null;
+            this.region = null;
+            this.address = null;
+            this.ctime = null;
+            this.mtime = null;
+        }
+
+
         public string ToJson(UserPayload user)
         {
-            return JsonConvert.SerializeObject(user);
+            return JsonConvert.SerializeObject(user,
+                            Newtonsoft.Json.Formatting.None,
+                            new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
         }
         public UserPayload Check()
         {
