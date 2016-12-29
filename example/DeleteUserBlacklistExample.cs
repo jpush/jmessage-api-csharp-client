083ace3b6aab55b6f4b2f9b265b672ca0fad57a7
fendouai <fendouai@gmail.com>
2016-12-29 15:21:24 +0800

    add examples

---
+++ example/DeleteUserBlacklistExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using jmessage.common;
+using jmessage.util;
+using jmessage.user;
+using jmessage;
+
+namespace example
+{
+    class DeleteUserBlacklistExample:JMessageExampleBase
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****开始修改用户信息******");
+            JMessageClient client = new JMessageClient(app_key, master_secret);
+            //需要修改的用户的用户名
+            string user = "jintian";
+            List<string> users = new List<string> { "jintianshigehaorizi" };
+            client._messageClient.deleteUserBlacklist(user, users);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ example/GetUsersExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using jmessage.common;
+using jmessage.util;
+using jmessage.user;
+using jmessage;
+
+namespace example
+{
+    class GetUsersExample : JMessageExampleBase
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****开始修改用户信息******");
+            JMessageClient client = new JMessageClient(app_key, master_secret);
+            UserPayload user = new UserPayload("jintian");
+            client._messageClient.getUsers(1,1);
+            Console.ReadLine();
+        }
+    }
+}
