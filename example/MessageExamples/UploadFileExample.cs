6ab9e3e597ce433db0cc3ce9c0ee359829d2c4dc
fendouai <fendouai@gmail.com>
2017-01-09 17:31:22 +0800

    add file client

--- example/MessageExamples/UploadFileExample.cs
+++ example/MessageExamples/UploadFileExample.cs
@@ -8,18 +8,26 @@ using System.Diagnostics;
 using Newtonsoft.Json;
 using jmessage.message;
 using jmessage;
+using System.Collections.Specialized;
 
 namespace example.MessageExamples
 {
-    class GetMediaExample
+    class UploadFileExample
     {
         public static String app_key = "6be9204c30b9473e87bad4dc";
         public static String master_secret = "a19bef7870c55d7e51f4c4f0";
         public static void Main(string[] args)
         {
             FileClient client = new FileClient(app_key, master_secret);
-            client.getMedia("qiniu/image/j/D5E5B73186ED3C10BFD9590B5BE2A821");
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("id", "TTR");
+            nvc.Add("btn-submit-photo", "Upload");
+            String content=client.HttpPostData("https://api.im.jpush.cn/v1/resource?type=image",600000, "touch-icon-iphone.png",
+                 @"C:\Users\fendouai\Desktop\touch-icon-iphone.png", nvc);
+            Console.WriteLine(content);
             Console.ReadLine();
         }
     }
 }
+
+
--- jmessage/message/FileClient.cs
+++ jmessage/message/FileClient.cs
@@ -6,6 +6,10 @@ using jmessage.util;
 using jmessage.common;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using log4net;
+using System.Collections.Specialized;
 
 namespace jmessage.message
 {
@@ -32,6 +36,86 @@ namespace jmessage.message
             url += mediaId;
             ResponseWrapper result = sendGet(url, Authorization(), null);
             return result;
+        }
+
+        public string HttpPostData(string url, int timeOut, string fileKeyName,
+                                            string filePath, NameValueCollection stringDict)
+        {
+            string responseContent;
+            var memStream = new MemoryStream();
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            // 边界符
+            var boundary = "---------------" + DateTime.Now.Ticks.ToString("x");
+            // 边界符
+            var beginBoundary = Encoding.ASCII.GetBytes("--" + boundary + "\r\n");
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            // 最后的结束符
+            var endBoundary = Encoding.ASCII.GetBytes("--" + boundary + "--\r\n");
+            webRequest.Headers.Add("Authorization", "Basic " + this.Authorization());
+            // 设置属性
+            webRequest.Method = "POST";
+            webRequest.Timeout = timeOut;
+            webRequest.ContentType = "multipart/form-data; boundary=" + boundary;
+
+            // 写入文件
+            const string filePartHeader =
+                "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n" +
+                 "Content-Type: application/octet-stream\r\n\r\n";
+            var header = string.Format(filePartHeader, fileKeyName, filePath);
+            var headerbytes = Encoding.UTF8.GetBytes(header);
+
+            memStream.Write(beginBoundary, 0, beginBoundary.Length);
+            memStream.Write(headerbytes, 0, headerbytes.Length);
+
+            var buffer = new byte[1024];
+            int bytesRead; // =0
+
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                memStream.Write(buffer, 0, bytesRead);
+            }
+
+            // 写入字符串的Key
+            var stringKeyHeader = "\r\n--" + boundary +
+                                   "\r\nContent-Disposition: form-data; name=\"{0}\"" +
+                                   "\r\n\r\n{1}\r\n";
+
+            foreach (byte[] formitembytes in from string key in stringDict.Keys
+                                             select string.Format(stringKeyHeader, key, stringDict[key])
+                                                 into formitem
+                                             select Encoding.UTF8.GetBytes(formitem))
+            {
+                memStream.Write(formitembytes, 0, formitembytes.Length);
+            }
+
+            // 写入最后的结束边界符
+            memStream.Write(endBoundary, 0, endBoundary.Length);
+
+            webRequest.ContentLength = memStream.Length;
+
+            var requestStream = webRequest.GetRequestStream();
+
+            memStream.Position = 0;
+            var tempBuffer = new byte[memStream.Length];
+            memStream.Read(tempBuffer, 0, tempBuffer.Length);
+            memStream.Close();
+
+            requestStream.Write(tempBuffer, 0, tempBuffer.Length);
+            requestStream.Close();
+
+            var httpWebResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            using (var httpStreamReader = new StreamReader(httpWebResponse.GetResponseStream(),
+                                                            Encoding.GetEncoding("utf-8")))
+            {
+                responseContent = httpStreamReader.ReadToEnd();
+            }
+
+            fileStream.Close();
+            httpWebResponse.Close();
+            webRequest.Abort();
+
+            return responseContent;
         }
 
         public string ToString(MessagePayload payload)
