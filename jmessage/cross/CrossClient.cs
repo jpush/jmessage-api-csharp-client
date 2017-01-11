d3e940f8a22cee79b8823de88278906ca8181e5a
fendouai <fendouai@gmail.com>
2017-01-11 14:35:08 +0800

    add CrossClient

--- jmessage/cross/CrossClient.cs
+++ jmessage/cross/CrossClient.cs
@@ -123,6 +123,103 @@ namespace jmessage.cross
             return result;
         }
 
+        public ResponseWrapper crossSetSingleNodisturb(string appkey, string username, LinkedList<string> add, LinkedList<string> remove)
+        {
+            String url = HOST_NAME_SSL;
+            url += CROSS_USER_PATH;
+            url += username;
+            url += "/nodisturb";
+            SingleNodisturbPayload single = new SingleNodisturbPayload(appkey, add, remove);
+            NodisturbPayload nodisturb = new NodisturbPayload();
+            nodisturb.single = single;
+            string payloadString = this.ToString(nodisturb);
+            ResponseWrapper result = sendPost(url, Authorization(), payloadString);
+            return result;
+        }
+
+        public ResponseWrapper crossSetGroupNodisturb(string appkey, string username,  LinkedList<int> add, LinkedList<int> remove)
+        {
+            String url = HOST_NAME_SSL;
+            url += CROSS_USER_PATH;
+            url += username;
+            url += "/nodisturb";
+            GroupNodisturbPayload group = new GroupNodisturbPayload(appkey, add, remove);
+            NodisturbPayload nodisturb = new NodisturbPayload();
+            nodisturb.group = group;
+            string payloadString = this.ToString(nodisturb);
+            ResponseWrapper result = sendPost(url, Authorization(), payloadString);
+            return result;
+        }
+
+        public ResponseWrapper crossAddFriends(string appkey, string username, List<string> users)
+        {
+            Preconditions.checkArgument(users != null, "pushPayload should not be empty");
+            string usersString = ToString(users);
+            Dictionary<string, string> payload = new Dictionary<string, string> { };
+            payload["appkey"] = appkey;
+            payload["users"] = usersString;
+            String payloadJson = this.ToString(payload);
+            return crossAddFriends(username, payloadJson);
+        }
+        public ResponseWrapper crossAddFriends(string username, string payloadString)
+        {
+            Preconditions.checkArgument(!string.IsNullOrEmpty(payloadString), "payloadString should not be empty");
+            Console.WriteLine(payloadString);
+            String url = HOST_NAME_SSL;
+            url += CROSS_USER_PATH;
+            url += username;
+            url += "/friends";
+            ResponseWrapper result = sendPost(url, Authorization(), payloadString);
+            return result;
+        }
+
+
+        public ResponseWrapper deleteFriends(string appkey, string username, List<string> users)
+        {
+            Preconditions.checkArgument(users != null, "pushPayload should not be empty");
+            string usersString = ToString(users);
+            Dictionary<string, string> payload = new Dictionary<string, string> { };
+            payload["appkey"] = appkey;
+            payload["users"] = usersString;
+            String payloadJson = this.ToString(payload);
+            return deleteFriends(username, payloadJson);
+        }
+        public ResponseWrapper deleteFriends(string username, string payloadString)
+        {
+            Preconditions.checkArgument(!string.IsNullOrEmpty(payloadString), "payloadString should not be empty");
+            Console.WriteLine(payloadString);
+            String url = HOST_NAME_SSL;
+            url += CROSS_USER_PATH;
+            url += username;
+            url += "/friends";
+            ResponseWrapper result = sendDelete(url, Authorization(), payloadString);
+            return result;
+        }
+
+        public ResponseWrapper putFriends(string appkey, string username, List<Dictionary<string, string>> users)
+        {
+            Preconditions.checkArgument(users != null, "pushPayload should not be empty");
+            foreach (Dictionary<string, string> user in users)
+            {
+                user["appkey"] = appkey;
+            }
+            String payloadJson = this.ToString(users);
+
+            return putFriends(username, payloadJson);
+        }
+
+        public ResponseWrapper putFriends(string username, string payload)
+        {
+            Preconditions.checkArgument(!string.IsNullOrEmpty(payload), "payloadString should not be empty");
+            Console.WriteLine(payload);
+            String url = HOST_NAME_SSL;
+            url += CROSS_USER_PATH;
+            url += username;
+            url += "/friends";
+            ResponseWrapper result = sendPut(url, Authorization(), payload);
+            return result;
+        }
+
         public string ToString(List<string> payload)
         {
             return JsonConvert.SerializeObject(payload,
@@ -132,7 +229,28 @@ namespace jmessage.cross
                                 NullValueHandling = NullValueHandling.Ignore
                             });
         }
+
+        public string ToString(NodisturbPayload payload)
+        {
+            return JsonConvert.SerializeObject(payload,
+                            Newtonsoft.Json.Formatting.None,
+                            new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
+        }
+
         public string ToString(List<Dictionary<string,string>> payload)
+        {
+            return JsonConvert.SerializeObject(payload,
+                            Newtonsoft.Json.Formatting.None,
+                            new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
+        }
+
+        public string ToString(Dictionary<string, string> payload)
         {
             return JsonConvert.SerializeObject(payload,
                             Newtonsoft.Json.Formatting.None,
---
+++ jmessage/cross/NodisturbPayload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jmessage.cross
+{
+    public class NodisturbPayload
+    {
+        public string appkey;
+        public SingleNodisturbPayload single;
+        public GroupNodisturbPayload group;
+        public NodisturbPayload()
+        {
+            this.single = null;
+            this.group = null;
+         }
+    }
+
+    public class SingleNodisturbPayload : NodisturbPayload
+    {
+        public LinkedList<string> add;
+        public LinkedList<string> remove;
+        public SingleNodisturbPayload(string appkey,LinkedList<string> add, LinkedList<string> remove)
+        {
+            this.appkey = appkey;
+            this.add = add;
+            this.remove = remove;
+        }
+    }
+    public class GroupNodisturbPayload : NodisturbPayload
+    {
+        public LinkedList<int> add;
+        public LinkedList<int> remove;
+        public GroupNodisturbPayload(string appkey, LinkedList<int> add, LinkedList<int> remove)
+        {
+            this.appkey = appkey;
+            this.add = add;
+            this.remove = remove;
+        }
+    }
+}
