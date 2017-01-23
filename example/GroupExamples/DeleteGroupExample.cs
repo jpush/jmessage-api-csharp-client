603febeedca03dc4c8ed90945be1be4c2066dc4c
fendouai <fendouai@gmail.com>
2017-01-23 15:29:53 +0800

    add group example

---
+++ example/GroupExamples/DeleteGroupExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using jmessage.util;
+using jmessage.common;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using jmessage.message;
+using jmessage.group;
+using jmessage;
+namespace example.GroupExamples
+{
+    class DeleteGroupExample
+    {
+        public static String app_key = "6be9204c30b9473e87bad4dc";
+        public static String master_secret = "a19bef7870c55d7e51f4c4f0";
+        public static void Main(string[] args)
+        {
+            GroupClient client = new GroupClient(app_key, master_secret);
+            client.deleteGroup(20293553);
+            Console.ReadLine();
+        }
+    }
+}
