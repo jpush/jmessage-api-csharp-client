5df613f09aa00223df80b801aae57b3cc22b2673
Hevin <liuhw@jpush.cn>
2017-06-19 19:00:22 +0800

    Optimize code of group

--- jmessage/group/MemberPayload.cs
+++ jmessage/group/MemberPayload.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace jmessage.group
 {
     public class MemberPayload
     {
-
         public List<string> add;
         public List<string> remove;
+
         public MemberPayload(List<string> add, List<string> remove)
         {
             this.add = add;
             this.remove = remove;
         }
     }
-    
-
 }
